f2fbe3bbddd281f9aaf39f93189960687183110c
Natalie Sakina Mbatha <83452606+NSM722@users.noreply.github.com>
2023-09-21 12:56:34 +0200

    Update extra.dart

--- lib/extra.dart
+++ lib/extra.dart
@@ -1,16 +1,18 @@
 import 'dart:async';
-import 'dart:convert';
+import 'dart:convert'; // converts the response body into a JSON Map with the 
 
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
+// function to fetch album from the internet
 Future<Album> fetchAlbum() async {
   final response = await http
       .get(Uri.parse('https://jsonplaceholder.typicode.com/albums/1'));
 
   if (response.statusCode == 200) {
-    // If the server did return a 200 OK response,
-    // then parse the JSON.
+    // If the server did return a 200 OK response
+    // then parse/covert the JSON Map into an album 
+    // using fromJson() factory method
     return Album.fromJson(jsonDecode(response.body));
   } else {
     // If the server did not return a 200 OK response,
@@ -19,6 +21,7 @@ Future<Album> fetchAlbum() async {
   }
 }
 
+// This Album class contains the data from the network request
 class Album {
   final int userId;
   final int id;
@@ -30,6 +33,7 @@ class Album {
     required this.title,
   });
 
+  // This class includes a factory constructor that creates an Album from JSON
   factory Album.fromJson(Map<String, dynamic> json) {
     return Album(
       userId: json['userId'],
@@ -54,6 +58,9 @@ class _MyAppState extends State<MyApp> {
   @override
   void initState() {
     super.initState();
+    // Storing the fetchAlbum() result in a state 
+    // variable ensures that the Future is executed 
+    // only once and then cached for subsequent rebuilds
     futureAlbum = fetchAlbum();
   }
 
@@ -69,7 +76,7 @@ class _MyAppState extends State<MyApp> {
           title: const Text('Fetch Data Example'),
         ),
         body: Center(
-          child: FutureBuilder<Album>(
+          child: FutureBuilder<Album>( // This widget simplifies working with async data
             future: futureAlbum,
             builder: (context, snapshot) {
               if (snapshot.hasData) {

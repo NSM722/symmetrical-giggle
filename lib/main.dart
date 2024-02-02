9ce640a167c6c05e02504085c78fe43a370e985c
Sakina Mbatha <sakina@natix.io>
2024-02-02 13:49:38 +0100

    refactor: remove debug mode banner

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'QuoteGenie Demo App',
       theme: ThemeData(
         primarySwatch: Colors.pink,
